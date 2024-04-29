3a02121f3f0ad8d9d749fcb4c0c3bcbe0a81e8ac
Madeline Moss <mcellis712@gmail.com>
2024-04-28 22:48:22 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+pub fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+
+        println!("{}", prompt);
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: T = match input.trim().parse() {
+            Ok(val) => val,
+            Err(_) => continue,
+        };
+
+        return input;
+    }
+}
